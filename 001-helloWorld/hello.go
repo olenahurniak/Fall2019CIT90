456c481e156bef657181dd419d51431377b10628
olenahurniak <olena.cz@gmail.com>
2019-08-21 18:57:28 -0700

    hello world

---
+++ 001-helloWorld/hello.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main()  {
+	fmt.Println("hello")
+
+	// const name, age = "Kim", 22
+	// fmt.Println(name, "is", age, "years old")
+}
+
+// conntrol flow
+/*
+Sequentionaly (top to bottom
+
+Conditionaly (if statment)
+Iteratively (loop)
+*/
+
