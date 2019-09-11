9071abd7f61dd8dcd5ddb6f63308e6faba6d9381
olenahurniak <olena.cz@gmail.com>
2019-09-11 15:15:17 -0700

    week 04 - composite literal map

---
+++ Assignments/week4/04-compositeLiteralMap.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+		// composite literal
+		oh := []string{"Lily,", "Armine"}
+		fmt.Println(oh)
+	
+		// composite literal
+	p := map[string]string{
+			"Lily": "Fresno,",
+			"Armine": "Clovis,",
+		}
+		fmt.Println(p)
+}
