1d95467d0a2a9ff21bc46a0c964da1856f7711dc
olenahurniak <olena.cz@gmail.com>
2019-12-04 14:43:23 -0800

    week14

---
+++ Assignments/week14-HMAC/crypto.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"crypto/hmac"
+	"crypto/sha512"
+	"fmt"
+)
+
+func signMessage(msg []byte) ([]byte, error) {
+	h := hmac.New(sha512.New, key)
+	_, err := h.Write(msg)
+	if err != nil {
+		return nil, fmt.Errorf("error in signMessage writing to hash %w", err)
+	}
+	signature := h.Sum(nil)
+	return signature, nil
+}
+
+func checkSig(msg, sig []byte) (bool, error) {
+	newSig, err := signMessage(msg)
+	if err != nil {
+		return false, fmt.Errorf("Error in checkSig while getting signature of message: %w", err)
+	}
+
+	same := hmac.Equal(newSig, sig)
+	return same, nil
+}
